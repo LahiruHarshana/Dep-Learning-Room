3d139d611dc28ff95f005000114adfbc20c8b33e
LahiruHarshana <lharshana2002@gmail.com>
2024-06-17 17:29:33 +0530

    updated

--- AJAX/js/index.js
+++ AJAX/js/index.js
@@ -1,4 +1,19 @@
 const btnXhrReadElm = document.getElementById("btn-xhr-read");
 btnXhrReadElm.addEventListener("click", () => {
-    alert("btn-xhr-read clicked");
+    //1. Let's create an instance of XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    //2. set up a call back function
+    xhr.addEventListener('readystatechange', () => {
+
+    });
+
+    //3. open the request
+    xhr.open('GET', 'http://ijse.lk',true);
+
+    //4. set additional headers and prepare the payload
+
+    //5. send the request
+
+
 });
