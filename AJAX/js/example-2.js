e5b4a72f6b52657f5328d00bbd48f8c1ef23f6c7
LahiruHarshana <lharshana2002@gmail.com>
2024-06-19 13:04:59 +0530

    updated

--- AJAX/js/example-2.js
+++ AJAX/js/example-2.js
@@ -1,4 +1,4 @@
-const downloadImage = 'https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg';
+const downloadImage = 'https://images.pexels.com/photos/24879566/pexels-photo-24879566.jpeg?cs=srgb&dl=pexels-kokorevas-24879566.jpg&fm=jpg&w=4000&h=5600&_gl=1*ufdq4l*_ga*NTkxMjM0ODA1LjE3MDM2ODE1MTY.*_ga_8JE65Q40S6*MTcxODc4MjQyOS4xMi4xLjE3MTg3ODI0MzMuMC4wLjA.';
 
 const imgElm = document.getElementById("img");
 document.getElementById("btn-download").addEventListener("click", () => {
@@ -6,6 +6,6 @@ document.getElementById("btn-download").addEventListener("click", () => {
     xhr.addEventListener('load', () => {
 
     });
-    xhr.open('DELETE', downloadImage,true);
+    xhr.open('GET', downloadImage,true);
     xhr.send();
 });
