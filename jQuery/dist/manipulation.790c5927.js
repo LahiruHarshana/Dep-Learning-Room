f8088366a147f32785d5cde59503846e64c4145b
LahiruHarshana <lharshana2002@gmail.com>
2024-06-20 12:31:25 +0530

    updated

--- jQuery/dist/manipulation.790c5927.js
+++ jQuery/dist/manipulation.790c5927.js
@@ -1,5 +1,6 @@
 const btnAppend = $("#btn-append");
 const btnPrepend = $("#btn-prepend");
 const btnInsertAfter = $("#btn-insert-after");
+const btnInsertBefore = $("#btn-insert-before");
 
 //# sourceMappingURL=manipulation.790c5927.js.map
--- jQuery/src/js/manupulation.js
+++ jQuery/src/js/manupulation.js
@@ -1,3 +1,4 @@
 const btnAppend = $('#btn-append');
 const btnPrepend = $('#btn-prepend');
 const btnInsertAfter = $('#btn-insert-after');
+const btnInsertBefore = $('#btn-insert-before');
