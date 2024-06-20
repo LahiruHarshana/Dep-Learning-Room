b001dadcd6537f5c2afb86a85455783ea6aa7ea1
LahiruHarshana <lharshana2002@gmail.com>
2024-06-20 10:59:48 +0530

    updated

--- jQuery/src/js/traversing.js
+++ jQuery/src/js/traversing.js
@@ -5,5 +5,5 @@ const li2Elm = $("#li-2");
 li2Elm.css('background-color','Yellow');
 //nextSibilingElement
 li2Elm.next().css('background-color','red');
-//parent
+//parentElm
 li2Elm.prev().css('background-color','red');
