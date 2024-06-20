d98dcabc4fb58a5e4dfc5a9ea35d52c52b380945
LahiruHarshana <lharshana2002@gmail.com>
2024-06-20 12:33:22 +0530

    updated

--- jQuery/dist/manipulation.790c5927.js
+++ jQuery/dist/manipulation.790c5927.js
@@ -2,5 +2,11 @@ const btnAppend = $("#btn-append");
 const btnPrepend = $("#btn-prepend");
 const btnInsertAfter = $("#btn-insert-after");
 const btnInsertBefore = $("#btn-insert-before");
+btnAppend.on("click", ()=>{
+    alert("append");
+});
+btnPrepend.on("click", ()=>{});
+btnInsertAfter.on("click", ()=>{});
+btnInsertBefore.on("click", ()=>{});
 
 //# sourceMappingURL=manipulation.790c5927.js.map
--- jQuery/src/js/manupulation.js
+++ jQuery/src/js/manupulation.js
@@ -1,4 +1,22 @@
 const btnAppend = $('#btn-append');
 const btnPrepend = $('#btn-prepend');
 const btnInsertAfter = $('#btn-insert-after');
-const btnInsertBefore = $('#btn-insert-before');
+const btnInsertBefore = $('#btn-insert-before')
+
+
+btnAppend.on('click',()=>{
+    alert('append');
+});
+
+btnPrepend.on('click',()=>{
+
+});
+
+btnInsertAfter.on('click',()=>{
+
+});
+
+btnInsertBefore.on('click',()=>{
+
+});
+
