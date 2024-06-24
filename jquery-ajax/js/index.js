3554b8c1466d7acabb92cc557455642a8ba17682
LahiruHarshana <lharshana2002@gmail.com>
2024-06-24 13:06:06 +0530

    updated

--- jquery-ajax/js/index.js
+++ jquery-ajax/js/index.js
@@ -1,1 +1,9 @@
-console.log(jquery===$);
+console.log(jQuery === $);
+
+$('#btn-send-get-request').on('click',()=>{
+    alert("working");
+});
+
+$('#btn-send-post-request').on('click',()=>{
+    alert("working");
+});
