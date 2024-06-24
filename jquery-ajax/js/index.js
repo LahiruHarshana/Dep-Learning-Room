5efb8eb01632847fc37ec0df9b7b9b565b82d0cf
LahiruHarshana <lharshana2002@gmail.com>
2024-06-24 16:47:06 +0530

    updated

--- jquery-ajax/js/index.js
+++ jquery-ajax/js/index.js
@@ -8,3 +8,27 @@ $('#btn-send-get-request').on('click',()=>{
         .catch(err => console.log("ERROR",err))
         .always(() => console.log("Nawa Gilunath ban choon"));
 })
+
+
+$('#btn-send-post-request').on('click', async ()=>{
+    const response = await fetch(URL ,{
+        method : 'POST',
+        body : JSON.stringify({
+            userId : 1,
+            title : "New To-do Ite",
+            completed : false
+        }),
+        headers :{
+            'Content-Type':'applocation/json'
+        }
+    });
+
+
+    const data = await $.ajax(URL,{
+        method: "POST",
+        data:JSON.stringify({
+            userId : 1 ,
+
+        })
+    })
+})
