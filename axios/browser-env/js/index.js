adad0fe261b072b17e46eb51e67757401606be14
LahiruHarshana <lharshana2002@gmail.com>
2024-06-25 11:29:25 +0530

    updated

---
+++ axios/browser-env/js/index.js
@@ -0,0 +1,45 @@
+console.log("axios" in globalThis);
+
+const btnSendGetRequest = document.getElementById('btn-send-get-request');
+
+const btnSendPostRequest = document.getElementById('btn-send-post-request');
+
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+btnSendPostRequest.addEventListener('click',async ()=>{
+    const response1 = await fetch(API_URL,{
+        method :'POST',
+        body : JSON.stringify({
+            userId :1,
+            title:"new Task",
+            completed : false
+        }),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+
+    const responseBody1 = await response1.json()
+    console.log("Fetch RESPONSE BODY :" ,responseBody1);
+
+
+
+    const response2 = await axios(API_URL,{
+        method: "POST",
+        data:{
+            userId :1,
+            title: "New Task",
+            completed: false
+        },
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
+});
+
+btnSendGetRequest.addEventListener('click', async ()=>{
+    const response1 = await fetch(API_URL);
+    console.log("FETCH DATA:" , await response1.json());
+
+    const response2 = await axios(API_URL);
+    console.log("AXIOS DATA",response2.data);
+});
