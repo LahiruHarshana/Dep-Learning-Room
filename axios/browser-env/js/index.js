2aabcea00d4edaa0dc73da2d053a9c85e15e0854
LahiruHarshana <lharshana2002@gmail.com>
2024-06-25 11:36:35 +0530

    updated

--- axios/browser-env/js/index.js
+++ axios/browser-env/js/index.js
@@ -5,6 +5,7 @@ console.log("axios" in globalThis);
     (config)=>{
         console.log("Now we can intercept the request before" +
             "it send to the server");
+        console.log('REQUEST:',config);
         return config;
     });
 
