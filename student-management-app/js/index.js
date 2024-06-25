417789ec7eef3d9f996f9316ecb2943cc1618db7
LahiruHarshana <lharshana2002@gmail.com>
2024-06-25 13:04:25 +0530

    updated

--- student-management-app/js/index.js
+++ student-management-app/js/index.js
@@ -5,6 +5,17 @@ async function loadAllStudents(){
 
     if (studentList.length){
         $('#tbl-student > tfoot').hide();
+        studentList.forEach(studentList=>{
+            const rowHtml =`
+        <tr>
+            <td>${studentList.id}</td>
+            <td>${studentList.name}</td>
+            <td>${studentList.contact}</td>
+            <td><i class="bi bi-trash"></i></td>
+        </tr>
+        `;
+        $('#tbl-student > tbody').append(rowHtml);
+        });
     }else{
 
     }
