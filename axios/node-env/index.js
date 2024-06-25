bfd2b17dfa4732df5495bf8ace24180582fe879e
LahiruHarshana <lharshana2002@gmail.com>
2024-06-25 11:58:58 +0530

    updated

--- axios/node-env/index.js
+++ axios/node-env/index.js
@@ -3,6 +3,21 @@ import axios from "axios";
 const API_URL = 'https://jsonplaceholder.typicode.com/todos';
 console.log(axios);
 
-const response1 = await axios.get(API_URL)
+const response1 = await axios(API_URL)
 
 console.log(response1.data);
+
+
+const response2 = await axios(API_URL,{
+    method : 'POST',
+    data :{
+        userId : 5,
+        title : "New Task",
+        completed:false
+    },
+    headers :{
+        'Content-Type' : 'application/json'
+    }
+});
+
+console.log(response2);
