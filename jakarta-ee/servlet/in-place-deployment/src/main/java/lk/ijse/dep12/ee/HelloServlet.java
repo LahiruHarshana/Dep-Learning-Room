fbd6f28df3f816e03b811f7c827c4112299286c4
LahiruHarshana <lharshana2002@gmail.com>
2024-06-27 18:33:31 +0530

    updated

---
+++ jakarta-ee/servlet/in-place-deployment/src/main/java/lk/ijse/dep12/ee/HelloServlet.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep12.ee;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author : L.H.J
+ * @File: HelloServlet
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-06-27, Thursday
+ **/
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter out = resp.getWriter()){
+            out.println("""
+                
+                
+                """);
+
+        }
+    }
+}
