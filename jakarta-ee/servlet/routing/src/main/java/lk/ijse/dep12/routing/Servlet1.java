76b5f7e5ee3300a61010e9408b71e0afa4f411c7
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 18:08:56 +0530

    updated

---
+++ jakarta-ee/servlet/routing/src/main/java/lk/ijse/dep12/routing/Servlet1.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep12.routing;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * @author : L.H.J
+ * @File: Servlet1
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-01, Monday
+ **/
+
+@WebServlet(name = "servlet1", urlPatterns = "/hello1")
+public class Servlet1 extends HttpServlet {
+}
---
+++ jakarta-ee/servlet/routing/src/main/java/lk/ijse/dep12/routing/Servlet2.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep12.routing;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * @author : L.H.J
+ * @File: Servlet2
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-01, Monday
+ **/
+
+@WebServlet
+public class Servlet2 extends HttpServlet {
+}
