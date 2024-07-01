b33eba13671416c49cd53a7366b07cfeca41477a
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 11:27:25 +0530

    updated

--- jakarta-ee/servlet/deployement-in-action/src/main/java/lk/ijse/dep/MyServlet.java
+++ jakarta-ee/servlet/deployement-in-action/src/main/java/lk/ijse/dep/MyServlet.java
@@ -1,7 +1,22 @@
-package lk.ijse.dep;/**
- * @File: MyServlet
+package lk.ijse.dep;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
  * @author : L.H.J
+ * @File: MyServlet
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-01, Monday
-**/public class MyServlet {
+ **/
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
 }
