9f65ebfdddcb150228dffbf7230feb8006095d2c
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 18:10:17 +0530

    updated

---
+++ jakarta-ee/servlet/routing/src/main/java/lk/ijse/dep12/routing/Servlet3.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep12.routing;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * @author : L.H.J
+ * @File: Servlet3
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-01, Monday
+ **/
+
+@WebServlet(name = "servlet1" ,urlPatterns = "/hello3" ,loadOnStartup = 0)
+public class Servlet3 extends HttpServlet {
+}
