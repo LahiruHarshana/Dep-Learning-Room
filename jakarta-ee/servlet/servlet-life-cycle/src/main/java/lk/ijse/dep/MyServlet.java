08edb5e11e865b382864d8cc41117ee8f244554a
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 13:09:55 +0530

    updated

--- jakarta-ee/servlet/servlet-life-cycle/src/main/java/lk/ijse/dep/MyServlet.java
+++ jakarta-ee/servlet/servlet-life-cycle/src/main/java/lk/ijse/dep/MyServlet.java
@@ -1,5 +1,8 @@
 package lk.ijse.dep;
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
@@ -10,10 +13,32 @@ import jakarta.servlet.http.HttpServlet;
  * @created : 2024-07-01, Monday
  **/
 
-@WebServlet(name = "my-servlet",urlPatterns = "./index.html")
+@WebServlet(name = "my-servlet",urlPatterns = "/hello",loadOnStartup = 0)
 public class MyServlet extends HttpServlet {
     static {
+        System.out.println("========================================================");
         System.out.println("My Servlet Class");
         System.out.println(Thread.currentThread().getName());
+        System.out.println("========================================================");
+
+    }
+
+    public MyServlet() {
+        System.out.println("========================================================");
+        System.out.println("My Servlet Constructor");
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("========================================================");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("========================================================");
+        System.out.println("My Servlet Init");
+        System.out.println("ServletContext :" + getServletContext());
+        System.out.println(Thread.currentThread().getName());
+        super.init();
+        System.out.println("My Boy just graduate to the servlet");
+        System.out.println("ServletContext :" + getServletContext());
+        System.out.println("========================================================");
     }
 }
