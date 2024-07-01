26f7144fc019e39e8d3a6fd6539abfacb5c708c7
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 17:46:01 +0530

    updated

--- jakarta-ee/servlet/servlet-life-cycle/src/main/java/lk/ijse/dep/MyServlet.java
+++ jakarta-ee/servlet/servlet-life-cycle/src/main/java/lk/ijse/dep/MyServlet.java
@@ -1,8 +1,6 @@
 package lk.ijse.dep;
 
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
@@ -15,30 +13,30 @@ import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "my-servlet",urlPatterns = "/hello",loadOnStartup = 0)
 public class MyServlet extends HttpServlet {
-    static {
-        System.out.println("========================================================");
-        System.out.println("My Servlet Class");
-        System.out.println(Thread.currentThread().getName());
-        System.out.println("========================================================");
 
+    static {
+        System.out.println("1. MyServlet class object is being loaded");
     }
 
-    public MyServlet() {
-        System.out.println("========================================================");
-        System.out.println("My Servlet Constructor");
-        System.out.println(Thread.currentThread().getName());
-        System.out.println("========================================================");
+    public MyServlet(){
+        System.out.println("2. MyServlet constroctor");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("3. GenericServlet init()");
+        try {
+            getServletContext();
+        }catch (Throwable t){
+            System.out.println("4. Yet to graduate");
+        }
+        super.init(config);
+        System.out.println("6. Exited:GenericServlet init()");
     }
 
     @Override
     public void init() throws ServletException {
-        System.out.println("========================================================");
-        System.out.println("My Servlet Init");
-        System.out.println("ServletContext :" + getServletContext());
-        System.out.println(Thread.currentThread().getName());
-        super.init();
-        System.out.println("My Boy just graduate to the servlet");
-        System.out.println("ServletContext :" + getServletContext());
-        System.out.println("========================================================");
+        System.out.println("5. init()");
+        System.out.println("5. Servlet Context: "+getServletContext());
     }
 }
