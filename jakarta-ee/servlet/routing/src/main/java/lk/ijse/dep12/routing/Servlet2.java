906ac49aadb6136ac12d0444f42b0702cd1dcfe6
LahiruHarshana <lharshana2002@gmail.com>
2024-07-01 18:09:26 +0530

    updated

--- jakarta-ee/servlet/routing/src/main/java/lk/ijse/dep12/routing/Servlet2.java
+++ jakarta-ee/servlet/routing/src/main/java/lk/ijse/dep12/routing/Servlet2.java
@@ -10,6 +10,6 @@ import jakarta.servlet.http.HttpServlet;
  * @created : 2024-07-01, Monday
  **/
 
-@WebServlet
+@WebServlet(name = "servlet2" ,urlPatterns = "/hello2")
 public class Servlet2 extends HttpServlet {
 }
