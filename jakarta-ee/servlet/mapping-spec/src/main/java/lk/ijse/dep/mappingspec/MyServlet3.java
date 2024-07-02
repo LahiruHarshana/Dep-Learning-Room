bbc037ade492e58f80286bdfb25c42f5960da35c
LahiruHarshana <lharshana2002@gmail.com>
2024-07-02 12:40:17 +0530

    updated

--- jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet3.java
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet3.java
@@ -1,6 +1,12 @@
 package lk.ijse.dep.mappingspec;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 /**
  * @author : L.H.J
@@ -8,5 +14,11 @@ import jakarta.servlet.http.HttpServlet;
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-02, Tuesday
  **/
+
+@WebServlet(name = "my-servlet-3",urlPatterns = "/dep/*.jsp")
 public class MyServlet3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Extension Mapping</h1>");
+    }
 }
