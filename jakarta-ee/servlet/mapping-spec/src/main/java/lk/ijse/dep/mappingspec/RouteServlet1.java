9a1768e0bcb580588d1acab63078eec5f109baea
LahiruHarshana <lharshana2002@gmail.com>
2024-07-02 13:09:37 +0530

    updated

--- jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/RouteServlet1.java
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/RouteServlet1.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep.mappingspec;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,6 +14,9 @@ import java.io.IOException;
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-02, Tuesday
  **/
+
+
+@WebServlet(name = "route-servlet-1",urlPatterns = "/route")
 public class RouteServlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
