5f623d52dee23d9329458efc278e136aa1992d88
LahiruHarshana <lharshana2002@gmail.com>
2024-07-02 12:59:20 +0530

    updated

---
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/RouteServlet3.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep.mappingspec;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author : L.H.J
+ * @File: RouteServlet1
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-02, Tuesday
+ **/
+
+@WebServlet(name = "route-servlet-3" , urlPatterns = "*.something")
+public class RouteServlet3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Route Servlet3: Extension Mapping Exact Mapping</h1>");
+    }
+}
