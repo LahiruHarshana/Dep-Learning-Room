b68d840e00e2be3b2f0abd2fd1b960ab93d2aedf
LahiruHarshana <lharshana2002@gmail.com>
2024-07-02 12:56:20 +0530

    updated

--- jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet4.java
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet4.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 
 /**
  * @author : L.H.J
- * @File: MyServlet3
+ * @File: MyServlet4
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-02, Tuesday
  **/
 
-@WebServlet(name = "my-servlet-3",urlPatterns = "*.jsp")
-public class MyServlet3 extends HttpServlet {
+@WebServlet(name = "my-servlet-4" , urlPatterns = "")
+public class MyServlet4 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>Extension Mapping</h1>");
+        resp.getWriter().println("<h1>Context Mapping</h1>");
     }
 }
--- jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/RouteServlet1.java
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/RouteServlet1.java
@@ -1,7 +1,6 @@
 package lk.ijse.dep.mappingspec;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,15 +9,13 @@ import java.io.IOException;
 
 /**
  * @author : L.H.J
- * @File: MyServlet3
+ * @File: RouteServlet1
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-02, Tuesday
  **/
-
-@WebServlet(name = "my-servlet-3",urlPatterns = "*.jsp")
-public class MyServlet3 extends HttpServlet {
+public class RouteServlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>Extension Mapping</h1>");
+        resp.getWriter().println("<h1>Route Servlet1: Exact Mapping</h1>");
     }
 }
