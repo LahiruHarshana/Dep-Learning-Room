d8bb21bb04e83e0c6f06a4dfdd5e4aef53acb0df
LahiruHarshana <lharshana2002@gmail.com>
2024-07-02 13:38:39 +0530

    updated

--- jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet2.java
+++ jakarta-ee/servlet/mapping-spec/src/main/java/lk/ijse/dep/mappingspec/MyServlet2.java
@@ -21,7 +21,7 @@ public class MyServlet2 extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1>Wildcard Mapping</h1>");
         resp.getWriter().println("<h1>URL:%s</h1>".formatted(req.getRequestURL()));
-        resp.getWriter().println("<h1>URL:%s</h1>".formatted(req.getRequestURI()));
+        resp.getWriter().println("<h1>URI:%s</h1>".formatted(req.getRequestURI()));
         resp.getWriter().println("<h1>ContextPath:%s</h1>".formatted(req.getContextPath()));
         resp.getWriter().println("<h1>ServletPath:%s</h1>".formatted(req.getServletPath()));
         resp.getWriter().println("<h1>PathInfo:%s</h1>".formatted(req.getPathInfo()));
