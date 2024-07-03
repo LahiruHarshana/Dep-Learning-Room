bc6b03859735cbd701d16cdcfc6a8e589e5cfdbd
LahiruHarshana <lharshana2002@gmail.com>
2024-07-03 11:51:23 +0530

    updated

---
+++ jakarta-ee/servlet/listeners/src/main/java/lk/ijse/dep/listeners/AppContextListener.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep.listeners;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+/**
+ * @author : L.H.J
+ * @File: AppContextListener
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-03, Wednesday
+ **/
+public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
