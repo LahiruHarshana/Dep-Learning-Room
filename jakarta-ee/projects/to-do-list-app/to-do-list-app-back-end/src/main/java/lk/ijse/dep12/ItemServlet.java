491d1d4d6e13a215c5dcba80ea3bfe8655133341
LahiruHarshana <lharshana2002@gmail.com>
2024-07-04 17:37:53 +0530

    updated

--- jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/ItemServlet.java
+++ jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/ItemServlet.java
@@ -1,17 +1,21 @@
 package lk.ijse.dep12;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 /**
  * @author : L.H.J
- * @File: MyServlet
+ * @File: ItemServlet
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-04, Thursday
  **/
 
-
-@WebServlet(name = "item-servlet",urlPatterns = "users/me/items/*")
+//@WebServlet(name = "item-servlet" , urlPatterns = "/")
 public class ItemServlet extends HttpServlet {
-    
+
 }
--- jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/UserServlet.java
+++ jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/UserServlet.java
@@ -1,57 +1,57 @@
 package lk.ijse.dep12;
 
-import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * @author : L.H.J
- * @File: MyServlet
+ * @File: UserServlet
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-04, Thursday
  **/
 
 
-@WebServlet(name = "user-servlet",urlPatterns = "/users/*")
+@WebServlet(name = "user-servlet" ,urlPatterns = "/users/*")
+@MultipartConfig(location = "/tmp",maxFileSize = 5 * 1024 * 1024)
 public class UserServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>SAVE USER</h1>");
-    }
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1>GET USER</h1>");
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>POST USER</h1>");
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        resp.getWriter().println("Name : " + name + "<br>");
+        resp.getWriter().println("Password : " + password + "<br>");
+        resp.getWriter().println("Email : " + email + "<br>");
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1>DELETE USER</h1>");
+    }
 
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>DELETE USER</h1>");
     }
 
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getMethod().equalsIgnoreCase("PATCH")){
-            doPatch(req,resp);
-        }else {
-            super.service(req,resp);
+        if (req.getMethod().equals("PATCH")) {
+            doPatch(req, resp);
+        } else {
+            super.service(req, resp);
         }
     }
-
-    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>PATCH USER</h1>");
-
-    }
-
 }
