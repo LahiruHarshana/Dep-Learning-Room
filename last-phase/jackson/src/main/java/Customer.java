2934e9b36ff0fc86b733ad91e33c22c34e5fe70c
LahiruHarshana <lharshana2002@gmail.com>
2024-07-05 11:26:22 +0530

    updated

---
+++ last-phase/jackson/src/main/java/Customer.java
@@ -0,0 +1,11 @@
+/**
+ * @author : L.H.J
+ * @File: Customer
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-05, Friday
+ **/
+public class Customer {
+    private Integer id;
+    private String name;
+    private String address;
+}
