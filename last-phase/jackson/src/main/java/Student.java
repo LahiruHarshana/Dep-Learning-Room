14e898723e1409d440f37ccd119f54c0a3f95bbe
LahiruHarshana <lharshana2002@gmail.com>
2024-07-05 12:10:10 +0530

    updated

--- last-phase/jackson/src/main/java/Student.java
+++ last-phase/jackson/src/main/java/Student.java
@@ -1,8 +1,10 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * @author : L.H.J
  * @File: Student
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-05, Friday
  **/
-public record Student(Integer id , String name , String contact , byte[] profilePicture) {
+public record Student(Integer id , String name , String contact , @JsonIgnore byte[] profilePicture) {
 }
