3ffe30c6220aebeaca66c42f2199e84fb761b9bb
LahiruHarshana <lharshana2002@gmail.com>
2024-07-05 12:15:45 +0530

    updated

---
+++ last-phase/jackson/src/main/java/TodoItem.java
@@ -0,0 +1,13 @@
+/**
+ * @author : L.H.J
+ * @File: TodoItem
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-05, Friday
+ **/
+public class TodoItem {
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private boolean completed;
+
+}
