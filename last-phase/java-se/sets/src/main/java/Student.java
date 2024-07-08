0414d95d0316fe6d79974b7d25754598e5c20150
LahiruHarshana <lharshana2002@gmail.com>
2024-07-08 13:48:30 +0530

    updated

---
+++ last-phase/java-se/sets/src/main/java/Student.java
@@ -0,0 +1,19 @@
+import lombok.*;
+
+/**
+ * @author : L.H.J
+ * @File: Customer
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-08, Monday
+ **/
+
+//@Data
+    @Setter
+    @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student{
+private int id;
+private String name;
+private int totalMarks;
+}
---
+++ last-phase/java-se/sets/src/main/java/setDemo5.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : L.H.J
+ * @File: setDemo5
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-08, Monday
+ **/
+public class setDemo5 {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "Kasun", 93);
+        Student s2 = new Student(1, "Nuwan", 86);
+        Student s3 = new Student(1, "Ruwan", 73);
+        Student s4 = new Student(1, "Supun", 21);
+        Student s5 = new Student(1, "Kamal", 19);
+
+
+        Set<Student> studentSet = new HashSet<>();
+        studentSet.add(s1);
+        studentSet.add(s2);
+        studentSet.add(s3);
+        studentSet.add(s4);
+        studentSet.add(s5);
+        studentSet.forEach(System.out::println);
+    }
+}
