245b6d2cecf17aaf8d8432afb5ce6af9ed334bf7
LahiruHarshana <lharshana2002@gmail.com>
2024-07-08 19:10:35 +0530

    updated

---
+++ last-phase/java-lang-features/annotation/src/main/java/DemoClass2.java
@@ -0,0 +1,17 @@
+/**
+ * @author : L.H.J
+ * @File: DemoClass2
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-08, Monday
+ **/
+public class DemoClass2 {
+    @DEP(studentCount = 10)
+    int x;
+
+    public static void printDepDetails(@DEP(studentCount = 20)String id,
+                                       @DEP(studentCount = 20) String name){
+
+        @DEP(studentCount = 20)
+        int x = 20;
+    }
+}
