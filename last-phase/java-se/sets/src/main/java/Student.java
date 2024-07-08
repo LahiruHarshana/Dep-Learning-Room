972b037f6b2f0308ba09d3613fe7a74a9601c318
LahiruHarshana <lharshana2002@gmail.com>
2024-07-08 13:56:24 +0530

    updated

--- last-phase/java-se/sets/src/main/java/Student.java
+++ last-phase/java-se/sets/src/main/java/Student.java
@@ -7,13 +7,16 @@ import lombok.*;
  * @created : 2024-07-08, Monday
  **/
 
-//@Data
-    @Setter
-    @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Student{
-private int id;
-private String name;
-private int totalMarks;
-}
+
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Student{
+        private int id;
+        private String name;
+        private int totalMarks;
+    }
--- last-phase/java-se/sets/src/main/java/setDemo5.java
+++ last-phase/java-se/sets/src/main/java/setDemo5.java
@@ -10,10 +10,10 @@ import java.util.Set;
 public class setDemo5 {
     public static void main(String[] args) {
         Student s1 = new Student(1, "Kasun", 93);
-        Student s2 = new Student(1, "Nuwan", 86);
-        Student s3 = new Student(1, "Ruwan", 73);
-        Student s4 = new Student(1, "Supun", 21);
-        Student s5 = new Student(1, "Kamal", 19);
+        Student s2 = new Student(2, "Nuwan", 86);
+        Student s3 = new Student(3, "Ruwan", 73);
+        Student s4 = new Student(4, "Supun", 21);
+        Student s5 = new Student(5, "Kamal", 19);
 
 
         Set<Student> studentSet = new HashSet<>();
@@ -21,6 +21,7 @@ public class setDemo5 {
         studentSet.add(s2);
         studentSet.add(s3);
         studentSet.add(s4);
+        studentSet.add(s5);
         studentSet.add(s5);
         studentSet.forEach(System.out::println);
     }
