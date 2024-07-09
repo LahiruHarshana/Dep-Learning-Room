975284f8ab7084f8e3ed93ded3e9515f0a032d7e
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 12:04:52 +0530

    updated

---
+++ last-phase/java-se/reflection-api/src/main/java/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * @author : L.H.J
+ * @File: Demo
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+public class Demo {
+    static int x;
+    int y;
+
+    static void myStaticMethod(){}
+    void myInstanceMethod(){}
+}
