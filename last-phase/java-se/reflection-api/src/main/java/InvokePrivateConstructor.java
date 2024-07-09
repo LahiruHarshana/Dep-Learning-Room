eccd52d95736d9b646ac76abbc2b596322bad0fa
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 13:40:29 +0530

    updated

---
+++ last-phase/java-se/reflection-api/src/main/java/InvokePrivateConstructor.java
@@ -0,0 +1,17 @@
+/**
+ * @author : L.H.J
+ * @File: InvokePrivateConstructor
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+public class InvokePrivateConstructor {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Demo3{
+    private Demo3(){
+        System.out.println("No one can invoke me");
+    }
+}
