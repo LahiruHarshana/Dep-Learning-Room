f195d320ea026d69ff40cfa08dedfc738dbb103a
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 20:23:53 +0530

    updated

--- last-phase/jakarta-ee/bean-validation/src/main/java/DobValidator.java
+++ last-phase/jakarta-ee/bean-validation/src/main/java/DobValidator.java
@@ -2,6 +2,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * @author : L.H.J
@@ -11,7 +12,10 @@ import java.time.LocalDate;
  **/
 public class DobValidator implements ConstraintValidator<Dob, LocalDate> {
     @Override
-    public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(LocalDate birthday, ConstraintValidatorContext constraintValidatorContext) {
+        LocalDate today = LocalDate.now();
+        int years = Period.between(today, birthday).getYears();
+        return years >= 18 && years <= 60;
 
     }
 }
