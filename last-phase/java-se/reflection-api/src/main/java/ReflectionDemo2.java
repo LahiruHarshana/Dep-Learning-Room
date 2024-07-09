8571b5acc01f5a76eea9b809b4c9f0ee413d6863
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 13:23:35 +0530

    updated

---
+++ last-phase/java-se/reflection-api/src/main/java/ReflectionDemo2.java
@@ -0,0 +1,17 @@
+/**
+ * @author : L.H.J
+ * @File: ReflectionDemo2
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+public class ReflectionDemo2 {
+
+}
+class Parent{
+
+}
+
+class Child extends Parent{
+    private int k;
+    public int m;
+}
