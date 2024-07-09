60f14e1cf0da2814624a74c843357db6b881bae7
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 11:10:30 +0530

    updated

---
+++ last-phase/java-lang-features/annotation/src/main/java/DemoClass4.java
@@ -0,0 +1,8 @@
+/**
+ * @author : L.H.J
+ * @File: DemoClass4
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+public class DemoClass4 {
+}
---
+++ last-phase/java-lang-features/annotation/src/main/java/MySimpleAnnotation.java
@@ -0,0 +1,15 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author : L.H.J
+ * @File: MySimpleAnnotation
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface MySimpleAnnotation {
+    
+}
