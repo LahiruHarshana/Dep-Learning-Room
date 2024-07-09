3b5aea02e7871b113add70cae8a0b5141c0d554b
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 13:26:47 +0530

    updated

--- last-phase/java-se/reflection-api/src/main/java/ReflectionDemo2.java
+++ last-phase/java-se/reflection-api/src/main/java/ReflectionDemo2.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Field;
+
 /**
  * @author : L.H.J
  * @File: ReflectionDemo2
@@ -5,7 +7,13 @@
  * @created : 2024-07-09, Tuesday
  **/
 public class ReflectionDemo2 {
-
+    public static void main(String[] args) {
+//        Field[] fields = Child.class.getFields();
+        Field[] declaredFields = Child.class.getDeclaredFields();
+        for (Field field : declaredFields) {
+            System.out.println(field);
+        }
+    }
 }
 class Parent{
 
