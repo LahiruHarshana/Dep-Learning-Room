b310d500be834539cf43f6082958fc1359eb92f2
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 16:14:16 +0530

    updated

--- last-phase/java-se/reflection-api/src/main/java/ReflectionWithAnnotation.java
+++ last-phase/java-se/reflection-api/src/main/java/ReflectionWithAnnotation.java
@@ -1,5 +1,6 @@
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -8,6 +9,7 @@ import java.lang.reflect.Method;
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-09, Tuesday
  **/
+
 @Dep12(startingDate = "2025-01-01")
 public class ReflectionWithAnnotation {
     @Dep12(startingDate = "2024-01-23")
@@ -20,20 +22,24 @@ public class ReflectionWithAnnotation {
         System.out.println("Starting Date: "+startingDate);
     }
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException {
-       Dep12 declaredAnnotations = ReflectionWithAnnotation.class.getDeclaredAnnotation(Dep12.class);
-        System.out.println(declaredAnnotations.startingDate());
-        System.out.println(declaredAnnotations.duration());
-        System.out.println("------------------------------------------");
-        Field st = ReflectionWithAnnotation.class.getDeclaredField("startingDate");
-        Dep12 declaredAnnotation = st.getDeclaredAnnotation(Dep12.class);
-        st.set(ReflectionWithAnnotation.class,declaredAnnotation.startingDate());
-        System.out.println(startingDate);
-        System.out.println("------------------------------------------");
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+//       Dep12 declaredAnnotations = ReflectionWithAnnotation.class.getDeclaredAnnotation(Dep12.class);
+//        System.out.println(declaredAnnotations.startingDate());
+//        System.out.println(declaredAnnotations.duration());
+//        System.out.println("------------------------------------------");
+//        Field st = ReflectionWithAnnotation.class.getDeclaredField("startingDate");
+//        Dep12 declaredAnnotation = st.getDeclaredAnnotation(Dep12.class);
+//        st.set(ReflectionWithAnnotation.class,declaredAnnotation.startingDate());
+//        System.out.println(startingDate);
+//        System.out.println("------------------------------------------");
+//
+//
+//        Method printDep12Details = ReflectionWithAnnotation.class.getDeclaredMethod("printDep12Details", int.class, String.class);
+//        Dep12 declaredAnnotation1 = printDep12Details.getDeclaredAnnotation(Dep12.class);
+//        int duration = declaredAnnotation1.duration();
+//        String start = declaredAnnotation1.startingDate();
+//
+//        printDep12Details.invoke(ReflectionWithAnnotation.class,duration,start);
 
-
-        Method printDep12Details = ReflectionWithAnnotation.class.getDeclaredMethod("printDep12Details", int.class, String.class);
-        Dep12 declaredAnnotation1 = printDep12Details.getDeclaredAnnotation(Dep12.class);
-        
     }
 }
