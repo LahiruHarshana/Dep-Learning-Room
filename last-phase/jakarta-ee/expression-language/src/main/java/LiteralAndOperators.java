3ef288e6b08f332c1928401bd212e3cc4a57c6a3
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 17:23:18 +0530

    updated

---
+++ last-phase/jakarta-ee/expression-language/src/main/java/LiteralAndOperators.java
@@ -0,0 +1,32 @@
+import jakarta.el.ExpressionFactory;
+import jakarta.el.StandardELContext;
+import jakarta.el.ValueExpression;
+
+/**
+ * @author : L.H.J
+ * @File: LiteralAndOperators
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-09, Tuesday
+ **/
+public class LiteralAndOperators {
+    public static void main(String[] args) {
+        ExpressionFactory ef = ExpressionFactory.newInstance();
+        StandardELContext context = new StandardELContext(ef);
+        ValueExpression ve1 = ef.createValueExpression(context,"${10}",int.class);
+        int value1 = ve1.getValue(context);
+        System.out.println(value1);
+
+        ValueExpression ve2 = ef.createValueExpression(context,"${true}",boolean.class);
+        boolean value2 = ve2.getValue(context);
+        System.out.println(value2);
+
+        ValueExpression ve3 = ef.createValueExpression(context,"${12.35}",float.class);
+        float value3 = ve3.getValue(context);
+        System.out.println(value3);
+
+
+        ValueExpression ve4 = ef.createValueExpression(context,"${'hello el literals'}",float.class);
+        float value4 = ve4.getValue(context);
+        System.out.println(value4);
+    }
+}
