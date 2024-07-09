6f72d8f015881b4afc23f4265a585ce2f69060aa
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 19:51:36 +0530

    updated

--- last-phase/jakarta-ee/bean-validation/src/main/java/Password.java
+++ last-phase/jakarta-ee/bean-validation/src/main/java/Password.java
@@ -1,9 +1,22 @@
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author : L.H.J
  * @File: Password
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-09, Tuesday
  **/
-public @interface Password {
 
+@Constraint(validatedBy = PasswordValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+    String message() default "Invalid password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
