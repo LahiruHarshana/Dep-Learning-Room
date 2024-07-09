9e45f59cb7bae3c261068e81a9a9ea2fa48540fd
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 17:12:46 +0530

    updated

--- last-phase/jakarta-ee/expression-language/src/main/java/ListWithEL2.java
+++ last-phase/jakarta-ee/expression-language/src/main/java/ListWithEL2.java
@@ -2,21 +2,19 @@ import jakarta.el.ExpressionFactory;
 import jakarta.el.StandardELContext;
 import jakarta.el.ValueExpression;
 
-import java.util.List;
-import java.util.Set;
-
 /**
  * @author : L.H.J
- * @File: SetWithEL
+ * @File: ListWithEL2
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-09, Tuesday
  **/
-public class SetWithEL {
+public class ListWithEL2 {
+
     public static void main(String[] args) {
         ExpressionFactory ef = ExpressionFactory.newInstance();
         StandardELContext context = new StandardELContext(ef);
-        ValueExpression ve = ef.createValueExpression(context,"${{10,20,30,30,40}}", Set.class);
-        Set<Long> set = ve.getValue(context);
-        set.forEach(System.out::println);
+        ValueExpression ve = ef.createValueExpression(context,"${['dep','cmjd','gdse'].get(1).toUpperCase()}", String.class);
+        String value = ve.getValue(context);
+        System.out.println(value);
     }
 }
