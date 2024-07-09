d97b5dc9f75a54ddb6ec86c18fbf430b689628c6
LahiruHarshana <lharshana2002@gmail.com>
2024-07-09 18:06:05 +0530

    updated

--- last-phase/jakarta-ee/expression-language/src/main/java/ELOperators3.java
+++ last-phase/jakarta-ee/expression-language/src/main/java/ELOperators3.java
@@ -13,6 +13,13 @@ public class ELOperators3 {
         context.getVariableMapper().setVariable("s1",ef.createValueExpression(s1,Student.class));
         context.getVariableMapper().setVariable("s2",ef.createValueExpression(s2,Student.class));
         context.getVariableMapper().setVariable("s3",ef.createValueExpression(s3,Student.class));
+
+        String value = ef.createValueExpression(context,
+                "#{[s1,s2,s3].map(s -> s.marks).max() gt s2.marks and s1.marks div 2 le s3.marks ?" +
+                "s1.id += '-' += s1.name.toUpperCase() += '-' += (s1.marks ge 75 ? 'A' : 'B'):}" +
+                "s2.id += '-' += s2.name += '-' += (s2.marks ge 75 ? 'A' : s2.marks >= 65 ? 'B' : 'C')}",
+                String.class).getValue(context);
+        System.out.println(value);
     }
 
     static class Student{
