fe49d72cda8e36782b968f49ae4fc59acee52fb4
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 13:08:00 +0530

    updated

--- jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/validation/PasswordValidator.java
+++ jakarta-ee/projects/to-do-list-app/to-do-list-app-back-end/src/main/java/lk/ijse/dep12/validation/PasswordValidator.java
@@ -3,6 +3,8 @@ package lk.ijse.dep12.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.HashSet;
+
 /**
  * @author : L.H.J
  * @File: PasswordValidator
@@ -11,7 +13,26 @@ import jakarta.validation.ConstraintValidatorContext;
  **/
 public class PasswordValidator implements ConstraintValidator<Password,String> {
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
 
+
+        if (password.length() < 6) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("Password must be at least 6 characters")
+                    .addConstraintViolation();
+            return false;
+        }
+        if (password.matches(".*[0-9].*")) return false;
+        if (password.matches(".*[A-Z].*")) return false;
+        if (password.matches(".*[a-z].*")) return false;
+        HashSet<Character> charachterSet = new HashSet<>();
+        for (char c : password.toCharArray()) charachterSet.add(c);
+        if (charachterSet.size() != password.length()) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("Password can't conatin repeat characters")
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
     }
 }
