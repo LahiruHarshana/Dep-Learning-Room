047c121199029147ad6452d60ccbf18e448c8c65
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 20:03:12 +0530

    updated

--- last-phase/jakarta-ee/jpa/02-native-hibernate/native-hibernate/hello-native-hibernate-rb-1/src/main/java/lk/ijse/dep12/jpa/HibernateUtil.java
+++ last-phase/jakarta-ee/jpa/02-native-hibernate/native-hibernate/hello-native-hibernate-rb-1/src/main/java/lk/ijse/dep12/jpa/HibernateUtil.java
@@ -1,7 +1,11 @@
 package lk.ijse.dep12.jpa;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 /**
  * @author : L.H.J
@@ -13,7 +17,19 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory =buildSessionFactory();
 
     private static SessionFactory buildSessionFactory(){
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("/application.properties")
+                .build();
 
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+                .build();
+
+        return sessionFactory;
     }
 
     public static SessionFactory getSessionFactory(){
