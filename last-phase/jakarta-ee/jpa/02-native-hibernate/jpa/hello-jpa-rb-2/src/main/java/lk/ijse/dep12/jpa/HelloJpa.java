0eeea46b644c1cdfd0316fc5c6198a4baf38b24f
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 19:16:24 +0530

    updated

--- last-phase/jakarta-ee/jpa/02-native-hibernate/jpa/hello-jpa-rb-2/src/main/java/lk/ijse/dep12/jpa/HelloJpa.java
+++ last-phase/jakarta-ee/jpa/02-native-hibernate/jpa/hello-jpa-rb-2/src/main/java/lk/ijse/dep12/jpa/HelloJpa.java
@@ -18,4 +18,5 @@ public class HelloJpa {
         }
         System.out.println("Entity Manager :"+em);
     }
+
 }
---
+++ last-phase/jakarta-ee/jpa/02-native-hibernate/jpa/hello-jpa-rb-2/src/main/java/lk/ijse/dep12/jpa/JpaUtil.java
@@ -0,0 +1,31 @@
+package lk.ijse.dep12.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author : L.H.J
+ * @File: JpaUtil
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-10, Wednesday
+ **/
+public class JpaUtil {
+    private static final EntityManagerFactory emf = buildEntityManagerFactory();
+    private static EntityManagerFactory buildEntityManagerFactory(){
+        Properties properties = new Properties();
+        try {
+            properties.load(JpaUtil.class.getResourceAsStream("/application.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Persistence.createEntityManagerFactory("dep-12",properties);
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory(){
+        return emf;
+    }
+}
