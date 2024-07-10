269c8cb2e36c2f9ea1d36a1772f17400b61915b2
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 17:09:07 +0530

    updated

---
+++ last-phase/jakarta-ee/jpa/hibernate-architecture/src/main/java/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * @File: Demo
+ * @author : L.H.J
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-10, Wednesday
+**/public class Demo {
+    public static void main(String[] args) {
+        SessionImpl impl = new SessionImpl();
+    }
+}
