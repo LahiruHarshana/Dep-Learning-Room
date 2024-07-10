c5be12b399932975648a07eee11cbefd84d2a036
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 17:05:53 +0530

    updated

---
+++ last-phase/jakarta-ee/jpa/hibernate-architecture/src/main/java/EntityManager.java
@@ -0,0 +1,11 @@
+/**
+ * @author : L.H.J
+ * @File: EntityManager
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-10, Wednesday
+ **/
+public interface EntityManager {
+    void persist();
+    void remove();
+    void refresh();
+}
---
+++ last-phase/jakarta-ee/jpa/hibernate-architecture/src/main/java/Session.java
@@ -0,0 +1,8 @@
+/**
+ * @author : L.H.J
+ * @File: Session
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-10, Wednesday
+ **/
+public interface Session extends EntityManager{
+}
