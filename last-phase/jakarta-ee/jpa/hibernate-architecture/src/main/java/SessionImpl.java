43e84961d8afc0934023e75c7509cc726fa04e81
LahiruHarshana <lharshana2002@gmail.com>
2024-07-10 17:08:30 +0530

    updated

--- last-phase/jakarta-ee/jpa/hibernate-architecture/src/main/java/SessionImpl.java
+++ last-phase/jakarta-ee/jpa/hibernate-architecture/src/main/java/SessionImpl.java
@@ -7,31 +7,31 @@
 public class SessionImpl implements Session{
     @Override
     public void persist() {
-        
+        System.out.println("Persist");
     }
 
     @Override
     public void remove() {
-
+        System.out.println("remove");
     }
 
     @Override
     public void refresh() {
-
+        System.out.println("refresh");
     }
 
     @Override
     public void save() {
-
+        System.out.println("save");
     }
 
     @Override
     public void saveAndUpdate() {
-
+        System.out.println("saveAndUpdate");
     }
 
     @Override
     public void delete() {
-
+        System.out.println("Demo");
     }
 }
