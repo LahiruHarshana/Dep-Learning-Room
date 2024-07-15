7f25958711e9eaa32831a3556d5419f5f5cb6520
LahiruHarshana <lharshana2002@gmail.com>
2024-07-15 11:43:43 +0530

    updated

--- last-phase/angular/components/src/app/card/card.component.ts
+++ last-phase/angular/components/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  @Input()
+  title = "Default card Title";
+  @Input()
+  image = "2.jpg";
 }
---
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-one/src/main/java/lk/ijse/dep12/jpa/relationship/HelloJpa.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep12.jpa.relationship;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import lk.ijse.dep12.jpa.relationship.util.JpaUtil;
+
+/**
+ * @author : L.H.J
+ * @File: HelloJpa
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-15, Monday
+ **/
+public class HelloJpa {
+
+    public static void main(String[] args) {
+        try(EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+            EntityManager em = emf.createEntityManager()){
+            System.out.println("Entity Manager Factory : "+emf);
+            System.out.println("Entity Manager :"+em);
+        }
+    }
+
+}
