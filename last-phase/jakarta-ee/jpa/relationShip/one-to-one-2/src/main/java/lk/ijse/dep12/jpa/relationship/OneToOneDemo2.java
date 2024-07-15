1f864ffcc7957efa6414b7e5ba6c3b8c52d7c312
LahiruHarshana <lharshana2002@gmail.com>
2024-07-15 15:22:07 +0530

    updated

---
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-one-2/src/main/java/lk/ijse/dep12/jpa/relationship/OneToOneDemo2.java
@@ -0,0 +1,39 @@
+package lk.ijse.dep12.jpa.relationship;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import lk.ijse.dep12.jpa.relationship.entity.Employee;
+import lk.ijse.dep12.jpa.relationship.entity.Spouse;
+import lk.ijse.dep12.jpa.relationship.util.JpaUtil;
+
+import java.util.List;
+
+/**
+ * @author : L.H.J
+ * @File: HelloJpa
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-15, Monday
+ **/
+public class OneToOneDemo2 {
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager()) {
+            System.out.println("EntityManagerFactory :"+emf);
+            System.out.println("EntityManager :" + em);
+
+            em.getTransaction().begin();
+
+            try {
+
+                Spouse sumana = em.find(Spouse.class,"S001");
+                System.out.println(sumana);
+
+
+                em.getTransaction().commit();
+            }catch (Throwable e){
+                em.getTransaction().rollback();
+                e.printStackTrace();
+            }
+        }
+    }
+}
