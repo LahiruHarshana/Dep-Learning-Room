9363f94da023d5a92453dc81f41df92a4e15d467
LahiruHarshana <lharshana2002@gmail.com>
2024-07-16 12:50:28 +0530

    updated

--- last-phase/jakarta-ee/jpa/relationShip/one-to-one-2/src/main/java/lk/ijse/dep12/jpa/relationship/entity/Account.java
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-one-2/src/main/java/lk/ijse/dep12/jpa/relationship/entity/Account.java
@@ -8,28 +8,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 
 /**
  * @author : L.H.J
- * @File: Customer
+ * @File: Account
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-16, Tuesday
  **/
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "customer")
-public class Customer {
+@Table(name = "account")
+public class Account {
     @Id
-    private String nic;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    private String address;
-    private String contact;
-    private Date dob;
+    String number;
+    String type;
+    BigDecimal balance;
+    @Column(name = "customer_nic")
+    String customerNic;
+    @Column(name = "open_date")
+    private Date openDate;
+    String username;
 }
