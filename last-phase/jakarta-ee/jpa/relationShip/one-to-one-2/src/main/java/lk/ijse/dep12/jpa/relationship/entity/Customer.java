cd918deae7f5b58a850e10a5c3510642e717be67
LahiruHarshana <lharshana2002@gmail.com>
2024-07-16 13:01:39 +0530

    updated

--- last-phase/jakarta-ee/jpa/relationShip/one-to-one-2/src/main/java/lk/ijse/dep12/jpa/relationship/entity/Customer.java
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-one-2/src/main/java/lk/ijse/dep12/jpa/relationship/entity/Customer.java
@@ -1,12 +1,7 @@
 package lk.ijse.dep12.jpa.relationship.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.sql.Date;
 
@@ -17,8 +12,9 @@ import java.sql.Date;
  * @created : 2024-07-16, Tuesday
  **/
 
+
+@ToString(exclude = "account")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "customer")
@@ -32,4 +28,28 @@ public class Customer {
     private String address;
     private String contact;
     private Date dob;
+    @OneToOne(mappedBy = "customer")
+    @Setter(AccessLevel.NONE)
+    private Account account;
+
+    public Customer(String nic, String firstName, String lastName, String address, String contact, Date dob, Account account) {
+        if (account != null && account.getCustomer() != this)
+            throw new IllegalStateException("Account is already associated with another customer");
+        this.nic = nic;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.contact = contact;
+        this.dob = dob;
+        this.account = account;
+    }
+
+    public Customer(String nic, String firstName, String lastName, String address, String contact, Date dob) {
+        this.nic = nic;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.contact = contact;
+        this.dob = dob;
+    }
 }
