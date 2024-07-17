e4c5b2f8a95145033b2f18705a784e3289486271
LahiruHarshana <lharshana2002@gmail.com>
2024-07-17 12:20:55 +0530

    updated

--- last-phase/jakarta-ee/jpa/relationShip/one-to-many/src/main/java/lk/ijse/dep12/jpa/relationship/HelloJpa.java
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-many/src/main/java/lk/ijse/dep12/jpa/relationship/HelloJpa.java
@@ -1,5 +1,10 @@
 package lk.ijse.dep12.jpa.relationship;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import lk.ijse.dep12.jpa.relationship.util.JpaUtil;
+
 /**
  * @author : L.H.J
  * @File: HelloJpa
@@ -7,4 +12,21 @@ package lk.ijse.dep12.jpa.relationship;
  * @created : 2024-07-17, Wednesday
  **/
 public class HelloJpa {
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+             EntityManager em = emf.createEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+
+                System.out.println("Entity Manager Factory :"+emf);
+                System.out.println("Entity Manager :"+em);
+
+                tx.commit();
+            }catch (Throwable t){
+                tx.rollback();
+                t.printStackTrace();
+            }
+        }
+    }
 }
