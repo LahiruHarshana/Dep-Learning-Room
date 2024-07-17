5a5f1f5f8c7c8d05fed81e6a5d5fdab59586ed8d
LahiruHarshana <lharshana2002@gmail.com>
2024-07-17 12:18:39 +0530

    updated

---
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-many/src/main/java/lk/ijse/dep12/jpa/relationship/HelloJpa.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep12.jpa.relationship;
+
+/**
+ * @author : L.H.J
+ * @File: HelloJpa
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-17, Wednesday
+ **/
+public class HelloJpa {
+}
