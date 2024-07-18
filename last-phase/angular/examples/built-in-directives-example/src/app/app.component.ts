6cb46b34daedff095f16f362addca3f7408a25a1
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 20:14:35 +0530

    updated

--- last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
+++ last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
@@ -1,31 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CommonModule, NgIf} from "@angular/common";
+import {CommonModule} from "@angular/common";
 
 //Type Alias
-type TodoItem={
-  id:number,
-  title:string,
-  completed:boolean
+type TodoItem = {
+  id: number,
+  title: String,
+  completed: boolean,
+  userId: number
 }
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf,CommonModule],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  // todoItems:{id:number,title:string,completed:boolean}[]=[];
-  todoItems:TodoItem[] = [];
+  findUser(userId:number){
+    return this.users.find(user => user.userID === userId);
+  }
+  users=[
+    {userID:1, name:'Kasun'},
+    {userID:2, name:'Piyumal'},
+    {userID:3, name:'Nuwan'},
+    {userID:4, name:'Supun'},
+    {userID:5, name:'Dasun'},
+  ]
+
+  todoItems: TodoItem[] = [];
   loading = true;
   constructor() {
     fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response=>response.json())
-      .then(todoItems=> this.todoItems = todoItems)
-      .finally(()=>setTimeout(()=>{
+      .then(response => response.json())
+      .then(todoItems => this.todoItems = todoItems)
+      .finally(() => setTimeout(() =>{
         this.loading = false;
-      },2000))
+      }, 2000));
   }
-  title = 'built-in-directives-example';
+  trackItemFn(index:number, item:TodoItem): number{
+    return item.id;
+  }
 }
