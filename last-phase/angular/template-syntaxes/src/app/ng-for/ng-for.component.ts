9c782e3e2bd97657082c5fface1e6a353ba6e0ca
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 16:56:35 +0530

    updated

--- last-phase/angular/template-syntaxes/src/app/ng-for/ng-for.component.ts
+++ last-phase/angular/template-syntaxes/src/app/ng-for/ng-for.component.ts
@@ -8,16 +8,16 @@ import {CommonModule} from "@angular/common";
   template: `
       <ul>
           <li *ngFor="let item of array1; let i = index">
-               {{i}}</li>
+              {{i}}</li>
       </ul>
       <hr>
       <ul>
           <li *ngFor="let item of array1;let isOdd = odd;
           let isFirst = first;let isLast=last;">
               {{item}} <span *ngIf="isFirst" class="text-danger"> FIRST</span>
-                       <span *ngIf="isLast" class="text-danger"> LAST</span>
-                       <span *ngIf="isOdd">- ODD</span>
-                       <span *ngIf="!isOdd">- EVEN</span>
+              <span *ngIf="isLast" class="text-danger"> LAST</span>
+              <span *ngIf="isOdd">- ODD</span>
+              <span *ngIf="!isOdd">- EVEN</span>
           </li>
       </ul>
 
@@ -31,12 +31,17 @@ import {CommonModule} from "@angular/common";
           </tr>
           </thead>
           <tbody>
-          <tr *ngFor="let customer of customers">
-              <td class="text-center">{{customer.id}}</td>
-              <td>{{customer.name}}</td>
-              <td>{{customer.address}}</td>
-              <td class="text-center"><i class="bi bi-trash"></i></td>
+          <tr *ngFor="let customer of customers;
+            trackBy: trackCustomerFn">
+              <td class="text-center">{{ customer.id }}</td>
+              <td>{{ customer.name }}</td>
+              <td>{{ customer.address }}</td>
+              <td class="text-center">
+                  <i (click)="deleteCustomer(customer.id)"
+                     class="bi bi-trash3-fill"></i>
+              </td>
           </tr>
+
           </tbody>
           <tfoot *ngIf="!customers.length">
           <tr>
@@ -46,11 +51,24 @@ import {CommonModule} from "@angular/common";
           </tr>
           </tfoot>
       </table>
+
+      <ul *ngFor="let item of items">
+          <li>{{item}}</li>
+      </ul>
+      <button (click)="items.pop()" class="btn btn-sm btn-danger">DELETE</button>
   `,
-  styles: ``
+  styles: [`
+
+  `]
 })
 export class NgForComponent {
-array1 = ['Kasun','Nuwan','Supun','Lahiru','Chamaru','Thisaru']
+
+  items = ['First','Second','Third','Fouth','Fifth']
+
+
+
+
+  array1 = ['Kasun','Nuwan','Supun','Lahiru','Chamaru','Thisaru']
   customers:{id:string, name:string, address:string}[] = [
     {id:"C001",name:"Lahiru Samopath",address:"Galle"},
     {id:"C002",name:"Lahiru harshana",address:"Matara"},
@@ -58,4 +76,11 @@ array1 = ['Kasun','Nuwan','Supun','Lahiru','Chamaru','Thisaru']
     {id:"C004",name:"Menaka Kasun",address:"panadura"},
     {id:"C005",name:"Kakshan harshana",address:"Matara"}
   ];
+  trackCustomerFn(index: number, customer: { id: String, name: String, address: String }) {
+    return customer.id;
+  }
+
+  deleteCustomer(customerId: String) {
+    this.customers = this.customers.filter(c => c.id!= customerId);
+  }
 }
