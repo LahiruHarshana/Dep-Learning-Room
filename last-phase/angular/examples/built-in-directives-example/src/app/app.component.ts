f207c79dad94a55291db82d2eb693e18d481b464
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 19:43:50 +0530

    updated

--- last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
+++ last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule, NgIf} from "@angular/common";
 
+//Type Alias
 type TodoItem={
-
+  id:number,
+  title:string,
+  completed:boolean
 }
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  todoItems:TodoItem=[];
+  // todoItems:{id:number,title:string,completed:boolean}[]=[];
+  todoItems:TodoItem[] = [];
+  loading = true;
   constructor() {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response=>response.json())
       .then(todoItems=> this.todoItems = todoItems)
+      .finally(()=>setTimeout(()=>{
+        this.loading = false;
+      },2000))
   }
   title = 'built-in-directives-example';
 }
