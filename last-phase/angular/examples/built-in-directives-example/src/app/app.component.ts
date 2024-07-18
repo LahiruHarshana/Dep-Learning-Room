233a1fbc6a7f577f2f66652ac407800896ce48ef
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 19:35:32 +0530

    updated

--- last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
+++ last-phase/angular/examples/built-in-directives-example/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type TodoItem={
+
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +12,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  todoItems:TodoItem=[];
+  constructor() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response=>response.json())
+      .then(todoItems=> this.todoItems = todoItems)
+  }
   title = 'built-in-directives-example';
 }
