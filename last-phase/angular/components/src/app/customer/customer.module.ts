a0f97759d6bdda39f5ebfb691220b18ddc352cd5
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 11:51:10 +0530

    updated

--- last-phase/angular/components/src/app/customer/customer.module.ts
+++ last-phase/angular/components/src/app/customer/customer.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { UpdateCustomerComponent } from '../update-customer/update-customer.component';
 
 
 
 @NgModule({
   declarations: [
-    AddCustomerComponent
+    AddCustomerComponent,
+    UpdateCustomerComponent
   ],
   imports: [
     CommonModule
