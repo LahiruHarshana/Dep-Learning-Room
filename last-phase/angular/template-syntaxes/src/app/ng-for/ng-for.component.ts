3d0b148c053de6666be7489984eb4f497a9325c2
LahiruHarshana <lharshana2002@gmail.com>
2024-07-18 15:47:56 +0530

    updated

--- last-phase/angular/template-syntaxes/src/app/ng-for/ng-for.component.ts
+++ last-phase/angular/template-syntaxes/src/app/ng-for/ng-for.component.ts
@@ -20,9 +20,40 @@ import {CommonModule} from "@angular/common";
                        <span *ngIf="!isOdd">- EVEN</span>
           </li>
       </ul>
+
+      <table class="table table-bordered table-hover">
+          <thead>
+          <tr>
+              <th class="text-center">ID</th>
+              <th>NAME</th>
+              <th>ADDRESS</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr *ngFor="let customer of customers">
+              <td>{{customer.id}}</td>
+              <td>{{customer.name}}</td>
+              <td>{{customer.address}}</td>
+          </tr>
+          </tbody>
+          <tfoot>
+          <tr>
+              <td colspan="3" class="text-center">
+                  No Customer Record Found
+              </td>
+          </tr>
+          </tfoot>
+      </table>
   `,
   styles: ``
 })
 export class NgForComponent {
 array1 = ['Kasun','Nuwan','Supun','Lahiru','Chamaru','Thisaru']
+  customers = [
+    {id:"C001",name:"Lahiru Samopath",address:"Galle"},
+    {id:"C001",name:"Lahiru harshana",address:"Matara"},
+    {id:"C001",name:"Saman harshana",address:"Gampaha"},
+    {id:"C001",name:"Menaka Kasun",address:"panadura"},
+    {id:"C001",name:"Kakshan harshana",address:"Matara"}
+  ]
 }
