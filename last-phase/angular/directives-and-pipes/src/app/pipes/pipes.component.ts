d82d7f8d14539f32c1103b64a515e51760c95cb6
LahiruHarshana <lharshana2002@gmail.com>
2024-07-19 14:17:59 +0530

    updated

--- last-phase/angular/directives-and-pipes/src/app/pipes/pipes.component.ts
+++ last-phase/angular/directives-and-pipes/src/app/pipes/pipes.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import {DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe} from "@angular/common";
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  PercentPipe,
+  TitleCasePipe,
+  UpperCasePipe
+} from "@angular/common";
 
 @Component({
   selector: 'app-pipes',
@@ -8,16 +16,46 @@ import {DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe} from "@angular/co
     UpperCasePipe,
     LowerCasePipe,
     TitleCasePipe,
-    DatePipe
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    PercentPipe
   ],
   template: `
-    <p>
+      @let myVariable = 10;
+    <p #pId>
       {{'pipes works!' | uppercase | lowercase  | titlecase}}
     </p>
-    <h1>Toady : {{today | date}}</h1>
+    <h1>Toady : {{today | date:'yyyy-MMMM-dd' | uppercase }}</h1>
+    <h1>Total: {{1257.2 | currency:' USD' :'symbol'}}</h1>
+    <h1>Number:{{myNumber | number:'1.2-2'}}</h1>
+    <h1>Customer ID : C{{customerId | number:'3.0'}}</h1>
+    <h1>Percentage: {{value | percent:'1.2-2'}}</h1>
+    Enter a number between 0 - 10 <br>
+    <input #txt type="text">
+    <button (click)="txt.focus()">Focus</button>
+    <button (click)="validate()">Validate</button>
+      <h1>Value: {{myVariable + 20}}</h1>
   `,
   styles: ``
 })
 export class PipesComponent {
   today = new Date();
+  myNumber = 12222222222233.12312312312123;
+  customerId = 10;
+  value = 0.8;
+
+  @ViewChild("txt")
+  txtElmRef!:ElementRef<HTMLInputElement>
+
+  validate() {
+    const value = +this.txtElmRef.nativeElement.value;
+    if (value >= 0 && value <= 10){
+      alert("validated");
+    }else{
+      alert("Invalidated");
+      this.txtElmRef.nativeElement.focus();
+      this.txtElmRef.nativeElement.select();
+    }
+  }
 }
