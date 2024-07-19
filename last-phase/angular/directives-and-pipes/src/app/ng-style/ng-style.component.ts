a04fce7acc803a43882473b80f35b0a15bf3fc62
LahiruHarshana <lharshana2002@gmail.com>
2024-07-19 12:11:56 +0530

    updated

--- last-phase/angular/directives-and-pipes/src/app/ng-style/ng-style.component.ts
+++ last-phase/angular/directives-and-pipes/src/app/ng-style/ng-style.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-ng-style',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <p>
       ng-style works!
