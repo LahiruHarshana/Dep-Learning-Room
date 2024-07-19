178b2d276235340e5693726ea84c9ac1b0fd0db7
LahiruHarshana <lharshana2002@gmail.com>
2024-07-19 11:29:49 +0530

    updated

--- last-phase/angular/directives-and-pipes/src/app/ng-class/ng-class.component.ts
+++ last-phase/angular/directives-and-pipes/src/app/ng-class/ng-class.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-ng-class',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      ng-class works!
-    </p>
+      <p [class]="strClasses" class="text-center">
+          Let's see how ngClass works<br>
+          <button (click)="strClasses = strClasses + 'second third'">Change the Strings</button>
+          <button (click)="strClasses =''">Clear</button>
+      </p>
+
+      <hr>
+
+      <p class="text-center" [class]="classesArray">
+          Let's see how ngClass work <br>
+      </p>
+
+      <hr>
+
+      <p class="text-center">
+          Let's see how ngClass work with an object
+      </p>
+
   `,
-  styles: ``
+  styles: `
+    .first { color: red; }
+    .second { background-color: yellow; }
+    .third { border: 1px solid red; }
+  `
 })
 export class NgClassComponent {
-
+  strClasses = 'first second third'; // Define the classes to be applied here
+  classesArray:string[] = ['first','second','third'];
+  classesObj ={
+    first:true,
+    second : false,
+    third : true
+  };
 }
