cdd807fba9d04e25144e543af74f8db933ecb21e
LahiruHarshana <lharshana2002@gmail.com>
2024-07-22 12:55:17 +0530

    updated

---
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-many-2/src/main/java/lk/ijse/dep12/jpa/relationship/OneToManyDemo2.java
@@ -0,0 +1,44 @@
+package lk.ijse.dep12.jpa.relationship;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import lk.ijse.dep12.jpa.relationship.entity.LawSuite;
+import lk.ijse.dep12.jpa.relationship.entity.Lawyer;
+import lk.ijse.dep12.jpa.relationship.util.JpaUtil;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author : L.H.J
+ * @File: HelloJpa
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-07-22, Monday
+ **/
+public class OneToManyDemo2 {
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+             EntityManager em = emf.createEntityManager()) {
+            EntityTransaction transaction = em.getTransaction();
+
+
+            try {
+                transaction.begin();
+
+                Lawyer tharindu = em.find(Lawyer.class, "L001");
+                LawSuite ls002 = em.find(LawSuite.class, "Ls-002");
+                ls002.setLawyer(tharindu);
+
+
+
+                transaction.commit();
+            }catch (Throwable t){
+                transaction.rollback();
+                t.printStackTrace();
+            }
+        }
+
+    }
+}
