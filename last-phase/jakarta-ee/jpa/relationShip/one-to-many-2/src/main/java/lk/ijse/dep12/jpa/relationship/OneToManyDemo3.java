f6c428970b403f8cf861e5c6b3e31cdc9022ab2e
LahiruHarshana <lharshana2002@gmail.com>
2024-07-22 12:56:59 +0530

    updated

--- last-phase/jakarta-ee/jpa/relationShip/one-to-many-2/src/main/java/lk/ijse/dep12/jpa/relationship/OneToManyDemo3.java
+++ last-phase/jakarta-ee/jpa/relationShip/one-to-many-2/src/main/java/lk/ijse/dep12/jpa/relationship/OneToManyDemo3.java
@@ -7,17 +7,13 @@ import lk.ijse.dep12.jpa.relationship.entity.LawSuite;
 import lk.ijse.dep12.jpa.relationship.entity.Lawyer;
 import lk.ijse.dep12.jpa.relationship.util.JpaUtil;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.List;
-
 /**
  * @author : L.H.J
  * @File: HelloJpa
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-07-22, Monday
  **/
-public class OneToManyDemo2 {
+public class OneToManyDemo3 {
     public static void main(String[] args) {
         try (EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
              EntityManager em = emf.createEntityManager()) {
@@ -26,10 +22,7 @@ public class OneToManyDemo2 {
 
             try {
                 transaction.begin();
-
-                Lawyer tharindu = em.find(Lawyer.class, "L001");
-                LawSuite ls002 = em.find(LawSuite.class, "Ls-002");
-                ls002.setLawyer(tharindu);
+                
 
                 transaction.commit();
             }catch (Throwable t){
