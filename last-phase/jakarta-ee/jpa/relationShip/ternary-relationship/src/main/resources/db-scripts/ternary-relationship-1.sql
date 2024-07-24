f0e891b85ebada01d17db3f8ef534e21bf7ec39a
LahiruHarshana <lharshana2002@gmail.com>
2024-07-24 13:52:19 +0530

    updated

--- last-phase/jakarta-ee/jpa/relationShip/ternary-relationship/src/main/resources/db-scripts/ternary-relationship-1.sql
+++ last-phase/jakarta-ee/jpa/relationShip/ternary-relationship/src/main/resources/db-scripts/ternary-relationship-1.sql
@@ -17,7 +17,8 @@ CREATE TABLE product (
 CREATE TABLE purchase (
                      customer_id VARCHAR(10) NOT NULL ,
                      supplier_id VARCHAR(10) NOT NULL ,
-                     product_code VARCHAR(255) NOT NULL,CONSTRAINT pk_purchase PRIMARY KEY (customer_id, supplier_id, product_code),
+                     product_code VARCHAR(255) NOT NULL,
+                     CONSTRAINT pk_purchase PRIMARY KEY (customer_id, supplier_id, product_code),
                      CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(id),
                      CONSTRAINT fk_product FOREIGN KEY (supplier_id) REFERENCES supplier(id),
                      CONSTRAINT fk_supplier FOREIGN KEY (product_code) REFERENCES product(code),
