356d98894d3f454417541bb11ef96bf4e0476177
LahiruHarshana <lharshana2002@gmail.com>
2024-07-24 19:24:58 +0530

    updated

--- last-phase/typescript/typescript-lang-2/dist/main.js
+++ last-phase/typescript/typescript-lang-2/dist/main.js
@@ -1,2 +1,20 @@
 "use strict";
 console.log("Hello Typescript language 2");
+console.log("Hello Typescript language 2");
+class Customer {
+    id = '';
+    name = '';
+    address = '';
+}
+class Student {
+    id;
+    name;
+    contact;
+    constructor(id, name, contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
+}
+var s001 = new Student("S001", "Tharindu", "073243423");
+console.log(s001.id, s001.name, s001.contact);
--- last-phase/typescript/typescript-lang-2/src/main.ts
+++ last-phase/typescript/typescript-lang-2/src/main.ts
@@ -1,1 +1,25 @@
 console.log("Hello Typescript language 2");
+console.log("Hello Typescript language 2");
+
+class Customer {
+    id:string = '';
+    name:string = '';
+    address:string = '';
+}
+
+class Student {
+    id: string;
+    name: string;
+    contact:string;
+
+
+    constructor(id: string, name: string, contact: string) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
+}
+
+var s001 = new Student("S001","Tharindu","073243423");
+
+console.log(s001.id,s001.name,s001.contact);
