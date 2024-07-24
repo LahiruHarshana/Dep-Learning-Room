a8c535103b63602b8109a0c1b7b5a5c45a31d242
LahiruHarshana <lharshana2002@gmail.com>
2024-07-24 17:43:17 +0530

    updated

--- last-phase/typescript/typescripts-2/src/main.ts
+++ last-phase/typescript/typescripts-2/src/main.ts
@@ -1,52 +1,93 @@
-console.log("hai");
+// console.log("hai");
 // 1. Type Annotations , Type Interfaces
-let x : number = 10;
+// let x : number = 10;
 // x ="ABC"
-
-
-//2. Type Shape
-const customer = {
-    id : 'C001',
-    name : 'Kasun Sampath',
-    address : "Galle"
-}
-console.log(customer.name);
+//
+//
+// //2. Type Shape
+// const customer = {
+//     id : 'C001',
+//     name : 'Kasun Sampath',
+//     address : "Galle"
+// }
+// console.log(customer.name);
 //
 // const employee :{id:string,name:string} = {
 //     id:"E001",
 //     name :"Lahiru"
+//  }
+//
+//
+// let employee : {
+//     id: number,
+//     name: string,
+//     gender : "male" | "female",
+//     address : string
 // }
+//
+// employee = {
+//     id : 1,
+//     name : "tharindu",
+//     gender : "male",
+//     address : "galle"
+// }
+//
+// let a  : 'dep' | 'cmjd ' | true | 10;
+// a = "dep"
+// a = "cmjd "
+//
+//
+// let myObj :{
+//     id:string,
+//     name:string
+//     print:()=> void
+// }
+//
+// myObj ={
+//     id : "C001",
+//     name : "Kasun Sampath",
+//     print(){
+//         console.log(this.id,this.name)
+//     }
+// }
+//
+// const resuilt = myObj.print();
+// console.log(resuilt)
+//
+//
+// type imantha = boolean;
+// let nidimathayi:imantha = true;
+
+//Union Types
+let asiri : string | number | boolean;
+asiri = 10;
+asiri =true
+
+// asiri = 10n
+
+// asiri = {}
 
 
-let employee : {
-    id: number,
-    name: string,
-    gender : "male" | "female",
-    address : string
+let myVar : number | undefined;
+// myVar = null;
+
+
+function myFunction(param1 : string , param2 :string) : string{
+    return param1 + param2;
+
 }
 
-employee = {
-    id : 1,
-    name : "tharindu",
-    gender : "male",
-    address : "galle"
+type Student = {
+    id : string,
+    name : string,
+    print : () => void
 }
 
-let a  : 'dep' | 'cmjd ' | true | 10;
-a = "dep"
-a = "cmjd "
+let myStudent;
+myStudent.id = 'C001';
+myStudent.name = 'Kasun';
+myStudent.print = myPrint;
 
-
-let myObj :{
-    id:string,
-    name:string
-    print:()=> void
-}
-
-myObj ={
-    id : "C001",
-    name : "Kasun Sampath",
-    print(){
-        console.log(this.id,this.name)
-    }
+function myPrint(){
+    
 }
