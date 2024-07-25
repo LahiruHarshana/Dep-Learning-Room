ae050f3ef647bcbe3b4366c7b96dfcfedfa82095
LahiruHarshana <lharshana2002@gmail.com>
2024-07-25 17:28:15 +0530

    upadated

--- last-phase/angular/binding/src/app/app.component.ts
+++ last-phase/angular/binding/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FirstComponent} from "./first/first.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  flag:boolean = false;
+  flag:boolean = true;
 }
