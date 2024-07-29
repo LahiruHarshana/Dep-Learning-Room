383e593360b82c19f32a6c82e989d314bd4a1e62
LahiruHarshana <lharshana2002@gmail.com>
2024-07-29 15:59:11 +0530

    updated

--- last-phase/angular/ng-router/src/app/app.routes.ts
+++ last-phase/angular/ng-router/src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {MainComponent} from "./main/main.component";
+import {HomeComponent} from "./home/home.component";
+import {FirstComponent} from "./first/first.component";
+import {SecondComponent} from "./second/second.component";
+import {ThirdComponent} from "./third/third.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/login'
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path:'main',
+    component:MainComponent,
+    children:[
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home'
+      },
+      {
+        path:'home',
+        component:HomeComponent
+      },
+      {
+        path:'first',
+        component:FirstComponent
+      },
+      {
+        path:'second',
+        component:SecondComponent
+      },
+      {
+        path:'third',
+        component:ThirdComponent
+      }
+    ]
+  }
+];
--- web-api/history-api/js/routing.js
+++ web-api/history-api/js/routing.js
@@ -9,3 +9,36 @@ btnBack.addEventListener('click',()=>{
 btnForward.addEventListener('click',()=>{
     history.forward();
 });
+
+const btnFirstPage = document.getElementById('btn-first-page');
+const btnSecondPage = document.getElementById('btn-second-page');
+
+
+btnFirstPage.addEventListener('click',()=>{
+    history.pushState('first',null,'first.html');
+    displayUI('first')
+});
+btnSecondPage.addEventListener('click',()=>{
+    history.pushState('second',null,'second.html');
+    displayUI('second')
+});
+
+
+const firstUi = document.getElementById('first-ui');
+const secondUi = document.getElementById('second-ui');
+addEventListener('popstate',(e)=> displayUI(e.state));
+
+
+
+function displayUI(state){
+    firstUi.classList.add('hide');
+    secondUi.classList.add('hide');
+    console.log("State: ",state?.state);
+    switch (state.state){
+        case "first":
+            break;
+        case "second":
+            break;
+    }
+};
+
