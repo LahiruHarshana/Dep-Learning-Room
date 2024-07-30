2d13c0f74ba8df3198f0aec2b708d87c5b616475
LahiruHarshana <lharshana2002@gmail.com>
2024-07-30 11:53:38 +0530

    updated

--- last-phase/angular/template-driven-forms/src/app/app.component.ts
+++ last-phase/angular/template-driven-forms/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TemplateFormComponent} from "./template-form/template-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   myInput = '';
+  isInputDirty = false;
 
   isInputValid():boolean {
     return /^[A-Za-z0-9 ]+$/.test(this.myInput)
   }
 
   validate() {
+    this.isInputDirty = true;
     if (this.isInputValid()){
       alert("Validated");
     }else {
