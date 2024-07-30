4897b942a54d3af19c1e3b8945bcbd5e95853381
LahiruHarshana <lharshana2002@gmail.com>
2024-07-30 13:04:56 +0530

    add standalone form conroll

--- last-phase/angular/template-driven-forms/src/app/customer-form/customer-form.component.ts
+++ last-phase/angular/template-driven-forms/src/app/customer-form/customer-form.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, isStandalone} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+
+type Customer = {
+  id : string,
+  name : string,
+  contact : string
+}
 
 @Component({
   selector: 'app-customer-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.css'
 })
 export class CustomerFormComponent {
-
+  customer : Customer = {
+    id:'C001',
+    name:'Tharindu',
+    contact:'234-3421341'
+  }
+  protected readonly isStandalone = isStandalone;
 }
