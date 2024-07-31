887f31db1e0c13db06369361cd233fe2043d928b
LahiruHarshana <lharshana2002@gmail.com>
2024-07-31 11:50:02 +0530

    updated

--- last-phase/angular/dynamic-forms/src/app/app.component.ts
+++ last-phase/angular/dynamic-forms/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dynamic-forms';
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    nic:['324323'],
+    name:[],
+    gender:[],
+    subjects:this.fb.array([
+      this.fb.group({
+        name :['Operating System'],
+        marks:['80']
+      }),
+      this.fb.group({
+        name :['DBMS'],
+        marks:['95']
+      })
+    ])
+    // subjects:this.fb.array([[]]),
+    // marks:this.fb.array([[]])
+  });
+
 }
