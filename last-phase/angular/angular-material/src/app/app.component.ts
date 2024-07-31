18bdb9cb986eea139bbfef8630ef75779bfe65ea
LahiruHarshana <lharshana2002@gmail.com>
2024-07-31 20:40:44 +0530

    updated

--- last-phase/angular/angular-material/src/app/app.component.ts
+++ last-phase/angular/angular-material/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ChildComponent} from "./child/child.component";
+import {HelloMaterialComponent} from "./hello-material/hello-material.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, HelloMaterialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
