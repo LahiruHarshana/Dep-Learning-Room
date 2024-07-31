a57d27af5c592a55a510cff9e4cbe3e204e8889e
LahiruHarshana <lharshana2002@gmail.com>
2024-07-31 20:32:06 +0530

    updated

---
+++ last-phase/angular/angular-material/src/app/hello-material/hello-material.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-material',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-material.component.html',
+  styleUrl: './hello-material.component.css'
+})
+export class HelloMaterialComponent {
+
+}
