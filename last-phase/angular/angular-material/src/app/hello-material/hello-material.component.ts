1f7e89f7e4b0a372c33b14f3e0e9e3fce23e7fda
LahiruHarshana <lharshana2002@gmail.com>
2024-07-31 21:23:59 +0530

    updated

--- last-phase/angular/angular-material/src/app/hello-material/hello-material.component.ts
+++ last-phase/angular/angular-material/src/app/hello-material/hello-material.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-
+import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import {MatFabAnchor, MatFabButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 @Component({
   selector: 'app-hello-material',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatIcon,
+    MatInput,
+    MatLabel,
+    MatHint,
+    MatFabButton,
+    MatFabAnchor,
+    RouterLink,
+
+
+  ],
   templateUrl: './hello-material.component.html',
   styleUrl: './hello-material.component.css'
 })
--- last-phase/angular/smart-pos/src/app/app.component.ts
+++ last-phase/angular/smart-pos/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ChildComponent} from "./child/child.component";
-import {HelloMaterialComponent} from "./hello-material/hello-material.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent, HelloMaterialComponent, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-material';
+  title = 'smart-pos';
 }
