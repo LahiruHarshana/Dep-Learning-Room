ca495d7fb0275be31aecc1d116d70d43b40e2ade
LahiruHarshana <lharshana2002@gmail.com>
2024-08-01 12:57:01 +0530

    updated

--- last-phase/angular/ng-firebase-todo-list/src/app/app.component.ts
+++ last-phase/angular/ng-firebase-todo-list/src/app/app.component.ts
@@ -1,16 +1,5 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButton, MatButtonModule} from "@angular/material/button";
-import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox";
-import {MatDatepicker, MatDatepickerModule, MatDatepickerToggle} from '@angular/material/datepicker';
-import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-import {provideNativeDateAdapter} from "@angular/material/core";
-import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from "@angular/material/chips";
-import {COMMA, ENTER} from "@angular/cdk/keycodes";
-import {LiveAnnouncer} from "@angular/cdk/a11y";
-import {MatMenuModule} from "@angular/material/menu";
 
 
 export interface Fruit {
@@ -19,63 +8,9 @@ export interface Fruit {
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [RouterOutlet, MatButtonModule, MatCheckboxModule, MatCheckboxModule, MatFormField, MatDatepickerToggle, MatDatepicker,MatFormFieldModule, MatInputModule, MatDatepickerModule,MatChipsModule, MatIconModule,MatMenuModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-
-  title = 'ng-firebase-todo-list';
-
-  readonly addOnBlur = true;
-  readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  readonly fruits = signal<Fruit[]>([{name: 'Lemon'}, {name: 'Lime'}, {name: 'Apple'}]);
-  readonly announcer = inject(LiveAnnouncer);
-
-  add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    // Add our fruit
-    if (value) {
-      this.fruits.update(fruits => [...fruits, {name: value}]);
-    }
-
-    // Clear the input value
-    event.chipInput!.clear();
-  }
-
-  remove(fruit: Fruit): void {
-    this.fruits.update(fruits => {
-      const index = fruits.indexOf(fruit);
-      if (index < 0) {
-        return fruits;
-      }
-
-      fruits.splice(index, 1);
-      this.announcer.announce(`Removed ${fruit.name}`);
-      return [...fruits];
-    });
-  }
-
-  edit(fruit: Fruit, event: MatChipEditedEvent) {
-    const value = event.value.trim();
-
-    // Remove fruit if it no longer has a name
-    if (!value) {
-      this.remove(fruit);
-      return;
-    }
-
-    // Edit existing fruit
-    this.fruits.update(fruits => {
-      const index = fruits.indexOf(fruit);
-      if (index >= 0) {
-        fruits[index].name = value;
-        return [...fruits];
-      }
-      return fruits;
-    });
-  }
 }
--- last-phase/angular/ng-firebase-todo-list/src/app/app.config.ts
+++ last-phase/angular/ng-firebase-todo-list/src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp(
+    {"projectId":"ng-firebase-todo-list-8c9a1",
+      "appId":"1:322297135233:web:395bf829ce4ea34e4b8d80",
+      "storageBucket":"ng-firebase-todo-list-8c9a1.appspot.com",
+      "apiKey":"AIzaSyDOmf-5Rn_v2rS8cipAMViTuPO6eGqEUTI",
+      "authDomain":"ng-firebase-todo-list-8c9a1.firebaseapp.com",
+      "messagingSenderId":"322297135233"
+    })), provideAuth(() => getAuth())]
 };
