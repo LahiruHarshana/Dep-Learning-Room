0a3fdb48eb0576b6295f48318be360cc99c6fdd8
LahiruHarshana <lharshana2002@gmail.com>
2024-08-01 11:43:17 +0530

    firebase ng project added

--- last-phase/angular/ng-firebase-todo-list/src/app/app.component.ts
+++ last-phase/angular/ng-firebase-todo-list/src/app/app.component.ts
@@ -1,13 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox";
+import {MatDatepicker, MatDatepickerModule, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {provideNativeDateAdapter} from "@angular/material/core";
+import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from "@angular/material/chips";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {MatMenuModule} from "@angular/material/menu";
 
+
+export interface Fruit {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [provideNativeDateAdapter()],
+  imports: [RouterOutlet, MatButtonModule, MatCheckboxModule, MatCheckboxModule, MatFormField, MatDatepickerToggle, MatDatepicker,MatFormFieldModule, MatInputModule, MatDatepickerModule,MatChipsModule, MatIconModule,MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+
   title = 'ng-firebase-todo-list';
+
+  readonly addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  readonly fruits = signal<Fruit[]>([{name: 'Lemon'}, {name: 'Lime'}, {name: 'Apple'}]);
+  readonly announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.update(fruits => [...fruits, {name: value}]);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(fruit: Fruit): void {
+    this.fruits.update(fruits => {
+      const index = fruits.indexOf(fruit);
+      if (index < 0) {
+        return fruits;
+      }
+
+      fruits.splice(index, 1);
+      this.announcer.announce(`Removed ${fruit.name}`);
+      return [...fruits];
+    });
+  }
+
+  edit(fruit: Fruit, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    this.fruits.update(fruits => {
+      const index = fruits.indexOf(fruit);
+      if (index >= 0) {
+        fruits[index].name = value;
+        return [...fruits];
+      }
+      return fruits;
+    });
+  }
 }
